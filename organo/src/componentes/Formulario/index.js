a8cbb8d5f7ba9236b2ff2509289ed411aceec8df
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-11-30 12:44:54 -0300

    adiciona componentes no projetos

---
+++ organo/src/componentes/Formulario/index.js
@@ -0,0 +1,29 @@
+import "./Formulario.css";
+import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
+
+const Formulario = () => {
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+  ];
+
+  return (
+    <section className="formulario">
+      <form>
+        <h1>Preencha os dados parar criar o card do colaborador.</h1>
+        <CampoTexto label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+        <ListaSuspensa label="Time" itens={times} />
+      </form>
+    </section>
+  );
+};
+
+export default Formulario;
