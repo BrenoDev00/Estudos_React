50b580042a8a71e5428b9ffa2b9edc2d1b9cd194
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-12-05 20:22:07 -0300

    adiciona useState na função App

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,11 +1,23 @@
 import Banner from "./componentes/Banner/index.js";
 import Formulario from "./componentes/Formulario/index.js";
+import { useState } from "react";
 
 function App() {
+  const [colaboradores, setColaboradores] = useState([]);
+
+  const aoNovoColaboradorCadastrado = (colaborador) => {
+    setColaboradores([...colaboradores, colaborador]);
+    console.log(colaborador);
+  };
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario
+        aoColaboradorCadastrado={(colaborador) =>
+          aoNovoColaboradorCadastrado(colaborador)
+        }
+      />
     </div>
   );
 }
--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -4,7 +4,7 @@ import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import Botao from "../Botao";
 
-const Formulario = () => {
+const Formulario = (props) => {
   const times = [
     "Programação",
     "Front-End",
@@ -22,7 +22,12 @@ const Formulario = () => {
 
   const aoSalvar = (evento) => {
     evento.preventDefault();
-    console.log("Valores submetidos: ", nome, cargo, imagem, time);
+    props.aoColaboradorCadastrado({
+      nome,
+      cargo,
+      imagem,
+      time,
+    });
   };
 
   return (
