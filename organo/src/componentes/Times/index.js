e6e3164d18a75bdafabea0794cf2a77ed38e3504
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-12-08 13:25:12 -0300

    adiciona cor primária e secundária dinamicamente no componente Time

--- organo/src/componentes/Times/index.js
+++ organo/src/componentes/Times/index.js
@@ -1,11 +1,11 @@
-import "./Time.css"
+import "./Time.css";
 
 const Time = (props) => {
-    return (
-        <section className="time">
-            <h2>{props.nome}</h2>
-        </section>
-    )
-}
+  return (
+    <section className="time" style={{ backgroundColor: props.corSecundaria }}>
+      <h2 style={{color: props.corPrimaria}}>{props.nome}</h2>
+    </section>
+  );
+};
 
 export default Time;
