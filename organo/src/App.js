2eac14554071e2affb8fc765d9052229e0fabccb
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-12-08 12:32:56 -0300

    adiciona o componente Time no projeto

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import Banner from "./componentes/Banner/index.js";
 import Formulario from "./componentes/Formulario/index.js";
-import { useState } from "react";
+import Time from "./componentes/Times/index.js";
 
 function App() {
   const [colaboradores, setColaboradores] = useState([]);
@@ -18,6 +19,13 @@ function App() {
           aoNovoColaboradorCadastrado(colaborador)
         }
       />
+      <Time nome="Programação" />
+      <Time nome="Front-End" />
+      <Time nome="Data Science" />
+      <Time nome="Devops" />
+      <Time nome="UX e Design" />
+      <Time nome="Mobile" />
+      <Time nome="Inovação e Gestão" />
     </div>
   );
 }
---
+++ organo/src/componentes/Times/index.js
@@ -0,0 +1,11 @@
+import "./Time.css"
+
+const Time = (props) => {
+    return (
+        <section className="time">
+            <h2>{props.nome}</h2>
+        </section>
+    )
+}
+
+export default Time;
