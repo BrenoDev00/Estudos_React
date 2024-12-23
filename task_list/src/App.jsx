0a452a344fbbbc8de9326fb0a19227a48805ac0e
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-12-23 20:08:33 -0300

    adicionando useState no app

--- task_list/src/App.jsx
+++ task_list/src/App.jsx
@@ -1,19 +1,20 @@
+import { useState } from "react";
 import { MainTitle } from "./components/MainTitle";
 import { TaskContainer } from "./components/taskContainer";
 
 function App() {
+  const [taskList, setTaskList] = useState([
+    "Estudar",
+    "Ir ao mercado",
+    "Jogar futebol",
+    "Andar de skate",
+  ]);
+
   return (
     <div className="h-screen bg-slate-800 flex justify-center text-white items-center">
       <div className="flex flex-col gap-8">
         <MainTitle title="Lista de Tarefas" />
-        <TaskContainer
-          taskList={[
-            "Estudar",
-            "Ir ao mercado",
-            "Jogar futebol",
-            "Andar de skate",
-          ]}
-        />
+        <TaskContainer taskList={taskList} />
       </div>
     </div>
   );
