d8d7b4cfaa2bc73f10f27a73142a356e96e6f48d
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2024-12-28 18:19:28 -0300

    adiciona opção de checkbox

--- task_list/src/components/TaskContainer/index.jsx
+++ task_list/src/components/TaskContainer/index.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import { TaskItem } from "../TaskItem";
 
 export const TaskContainer = ({ taskList, removeTask }) => {
+  const [checked, setChecked] = useState(false);
+
   return taskList.length > 0 ? (
     <main className="-translate-y-6">
+      <input
+        type="checkbox"
+        name="border-bottom"
+        id="border-btn"
+        checked={checked}
+        onClick={() => setChecked(!checked)}
+      />
+      <label htmlFor="border-bottom">Habilitar linhas</label>
+
       <section className="border-2 border-violet-900 w-[500px] flex flex-col gap-3 p-3">
         {taskList.map((task, index) => (
           <TaskItem
             key={`task-item-${index}`}
             task={task}
             removeTask={removeTask}
+            showBorderBottom={checked}
           />
         ))}
       </section>
--- task_list/src/components/TaskItem/index.jsx
+++ task_list/src/components/TaskItem/index.jsx
@@ -1,6 +1,10 @@
-export const TaskItem = ({ task, removeTask }) => {
+export const TaskItem = ({ task, removeTask, showBorderBottom }) => {
   return (
-    <article className="flex gap-4 justify-between items-center">
+    <article
+      className={`flex gap-4 justify-between items-center ${
+        showBorderBottom ? "border-b-2 border-slate-600 pb-[15px]" : ""
+      }`}
+    >
       <p>{task}</p>
       <button
         onClick={() => removeTask(task)}
