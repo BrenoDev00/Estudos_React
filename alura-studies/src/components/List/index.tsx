f0aae734f58ff43e44c100898faa0277c19d22ce
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-01-14 13:26:01 -0300

    renderizando listas de forma dinâmica

--- alura-studies/src/components/List/index.tsx
+++ alura-studies/src/components/List/index.tsx
@@ -1,20 +1,30 @@
 import React from "react";
 
 function List() {
+  const tasks = [
+    {
+      name: "React",
+      time: "02:00:00",
+    },
+    {
+      name: "JavaScript",
+      time: "01:00:00",
+    },
+  ];
+
   return (
     <aside>
       <h2>Estudos do dia</h2>
 
       <ul>
-        <li>
-          <h3>React</h3>
-          <span>02:00:00</span>
-        </li>
-
-        <li>
-          <h3>JavaScript</h3>
-          <span>01:00:00</span>
-        </li>
+        {tasks.map((task, index) => {
+          return (
+            <li key={index}>
+              <h3>{task.name}</h3>
+              <span>{task.time}</span>
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
