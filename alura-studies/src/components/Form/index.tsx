909f4389106a76e85bfeb55c59bf531da8980dbc
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-01-18 12:08:40 -0300

    aplicando useState na aplicação

--- alura-studies/src/components/Form/index.tsx
+++ alura-studies/src/components/Form/index.tsx
@@ -2,9 +2,21 @@ import React from "react";
 import Button from "../Button";
 
 class Form extends React.Component {
+  state = {
+    task: "",
+    time: "00:00",
+  };
+
+  addTask(event: React.FormEvent) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form className="flex flex-col  gap-4 w-[600px] bg-sky-800 rounded-md p-4">
+      <form
+        className="flex flex-col  gap-4 w-[600px] bg-sky-800 rounded-md p-4"
+        onSubmit={(event) => this.addTask(event)}
+      >
         <div className="flex flex-col gap-2">
           <label className="text-white text-2xl" htmlFor="task">
             Adicione um novo estudo
@@ -13,6 +25,10 @@ class Form extends React.Component {
             className="p-2 rounded-[5px] outline-none"
             type="text"
             name="task"
+            value={this.state.task}
+            onChange={(event) =>
+              this.setState({ ...this.state, task: event.target.value })
+            }
             placeholder="O que você quer estudar"
             required
           />
@@ -26,6 +42,10 @@ class Form extends React.Component {
             className="p-2 rounded-[5px] outline-none"
             type="time"
             name="time"
+            value={this.state.time}
+            onChange={(event) =>
+              this.setState({ ...this.state, time: event.target.value })
+            }
             step="1"
             min="00:00:00"
             max="01:30:00"
--- alura-studies/src/components/List/index.tsx
+++ alura-studies/src/components/List/index.tsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import Task from "./Task";
 
 function List() {
-  const tasks = [
+  const [task, setTask] = useState([
     {
       name: "React",
       time: "02:00:00",
@@ -10,14 +11,21 @@ function List() {
       name: "JavaScript",
       time: "01:00:00",
     },
-  ];
+  ]);
 
   return (
     <aside>
-      <h2 className="text-white text-2xl text-center">Estudos do dia</h2>
+      <h2
+        onClick={() => {
+          setTask([...task, { name: "Estudar estado", time: "05:00:00" }]);
+        }}
+        className="text-white text-2xl text-center"
+      >
+        Estudos do dia
+      </h2>
 
       <ul className="text-white flex flex-col gap-4 mt-[20px]">
-        {tasks.map((task, index) => {
+        {task.map((task, index) => {
           return <Task key={index} task={task.name} time={task.time} />;
         })}
       </ul>
