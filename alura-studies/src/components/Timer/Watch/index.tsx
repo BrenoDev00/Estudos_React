4c2c0ec588efaee6b74bb6ac888983d30954d7a7
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-01-18 11:17:38 -0300

    adiciona estilização no componente Watch

--- alura-studies/src/components/Timer/Watch/index.tsx
+++ alura-studies/src/components/Timer/Watch/index.tsx
@@ -1,6 +1,6 @@
 export function Watch() {
   return (
-    <div className="text-white bg-slate-600 p-4 rounded-md text-xl text-center">
+    <div className="text-white bg-slate-600 p-4 rounded-md text-2xl text-center flex gap-1 justify-center">
       <span className="p-1 rounded-md bg-slate-800">0</span>
       <span className="p-1 rounded-md bg-slate-800">0</span>
       <span className="px-1">:</span>
