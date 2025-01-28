8f905b5e220b508033917528ab51d0e84c8d5859
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-01-28 18:17:06 -0300

    adiciona contador de compras

--- e-commerce/src/components/UserButtons.tsx
+++ e-commerce/src/components/UserButtons.tsx
@@ -3,8 +3,15 @@ import { FaUser, FaShoppingBag } from "react-icons/fa";
 export const UserButtons = () => {
   return (
     <div className="flex gap-4 items-center">
-      <button>
+      <button className="px-2 relative">
         <FaShoppingBag />
+
+        <div
+          id="cart-amount"
+          className="absolute inline-flex items-center justify-center w-6 h-6 font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2"
+        >
+          3
+        </div>
       </button>
 
       <a>
