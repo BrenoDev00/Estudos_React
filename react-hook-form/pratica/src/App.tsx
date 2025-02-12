8eb96898ea17eb280efa3ebdcef879df5596e0b3
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-02-12 18:03:20 -0300

    corrige bug de css

--- react-hook-form/pratica/src/App.tsx
+++ react-hook-form/pratica/src/App.tsx
@@ -15,25 +15,27 @@ function App() {
   };
 
   return (
-    <main className="bg-slate-800 flex flex-col">
-      <Form
-        onSubmitForm={(user) => {
-          registerUser(user);
-        }}
-      />
+    <div className="bg-slate-800 h-dvh">
+      <main className="bg-slate-800">
+        <Form
+          onSubmitForm={(user) => {
+            registerUser(user);
+          }}
+        />
 
-      {onRegisteredUser.length > 0 && (
-        <section className="m-auto max-w-[1200px] py-4 px-2 flex flex-col items-center gap-4 justify-center border border-white mt-20">
-          <h2 className="text-slate-200 text-2xl">Usuários cadastrados</h2>
+        {onRegisteredUser.length > 0 && (
+          <section className="m-auto max-w-[1200px] py-4 px-2 flex flex-col items-center gap-4 justify-center mt-20">
+            <h2 className="text-slate-200 text-2xl">Usuários cadastrados</h2>
 
-          <div className="flex gap-6 mt-4 flex-wrap ">
-            {onRegisteredUser.map((user) => {
-              return <Card {...user} />;
-            })}
-          </div>
-        </section>
-      )}
-    </main>
+            <div className="flex gap-6 mt-4 flex-wrap ">
+              {onRegisteredUser.map((user) => {
+                return <Card {...user} />;
+              })}
+            </div>
+          </section>
+        )}
+      </main>
+    </div>
   );
 }
 
