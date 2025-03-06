61394ad513cd4a3e86409b50835a50f72f744cd2
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-03-06 17:56:33 -0300

    verifica a existência de elementos html com cypress

--- cypress/bytebank/front-end/cypress/e2e/inicio.cy.js
+++ cypress/bytebank/front-end/cypress/e2e/inicio.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Página inicial', () => {
+  it('Deve renderizar a tag h1 com o texto correto', () => {
+    // visit: visitar determinada url
     cy.visit('http://localhost:3000');
+
+    // get: obter elemento pelo DOM; should('exist'): verifica se um elemento está renderizado no DOM; contains: verifica a existência de determinado texto.
+    cy.get('h1[data-test="titulo-principal"')
+      .should('exist')
+      .contains(
+        'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
+      );
   });
 });
