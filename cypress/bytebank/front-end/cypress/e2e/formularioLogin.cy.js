fe65d677c633df92162aa137be79a85fcabd5b6c
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-03-10 11:31:02 -0300

    adiciona mais teste para casos de uso do formulário login

--- cypress/bytebank/front-end/cypress/e2e/formularioLogin.cy.js
+++ cypress/bytebank/front-end/cypress/e2e/formularioLogin.cy.js
@@ -3,6 +3,23 @@ describe('Formulário de Login', () => {
     cy.visit('http://localhost:3001');
   });
 
+  it('Não deve permitir enviar formulário com todos os campos vazios', () => {
+    cy.getByDataTest('botao-login').click();
+    cy.getByDataTest('botao-enviar').click();
+    cy.getByDataTest('mensagem-erro')
+      .should('exist')
+      .contains('O campo de senha é obrigatório');
+  });
+
+  it('Não deve permitir o campo de e-mail vazio', () => {
+    cy.getByDataTest('botao-login').click();
+    cy.getByDataTest('senha-input').type('123456');
+    cy.getByDataTest('botao-enviar').click();
+    cy.getByDataTest('mensagem-erro')
+      .should('exist')
+      .contains('O campo email é obrigatório');
+  });
+
   it('Não deve permitir um e-mail inválido', () => {
     cy.getByDataTest('botao-login').click(); // click: clicar em um elemento.
     cy.getByDataTest('email-input').type('neilton@alura'); // type: digitar em um input.
@@ -11,5 +28,14 @@ describe('Formulário de Login', () => {
     cy.getByDataTest('mensagem-erro')
       .should('exist')
       .contains('O email digitado é inválido');
+  });
+
+  it('Não deve permitir o campo de senha vazio', () => {
+    cy.getByDataTest('botao-login').click();
+    cy.getByDataTest('email-input').type('neilton@alura.com');
+    cy.getByDataTest('botao-enviar').click();
+    cy.getByDataTest('mensagem-erro')
+      .should('exist')
+      .contains('O campo de senha é obrigatório');
   });
 });
