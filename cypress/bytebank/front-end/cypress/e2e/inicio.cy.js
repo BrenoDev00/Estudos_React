cc50d7fe06688eb2930a77fa7b38be4cdc0c7469
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2025-03-10 09:54:13 -0300

    adiciona comandos personalizados com cypress

--- cypress/bytebank/front-end/cypress/e2e/inicio.cy.js
+++ cypress/bytebank/front-end/cypress/e2e/inicio.cy.js
@@ -1,13 +1,14 @@
 describe('Página inicial', () => {
-  it('Deve renderizar a tag h1 com o texto correto', () => {
+  // beforeEach: executa este comando antes de iniciar qualquer teste.
+  beforeEach(() => {
     // visit: visitar determinada url
-    cy.visit('http://localhost:3000');
+    cy.visit('http://localhost:3001');
+  });
 
+  it('Deve renderizar a tag h1 com o texto correto', () => {
     // get: obter elemento pelo DOM; should('exist'): verifica se um elemento está renderizado no DOM; contains: verifica a existência de determinado texto.
-    cy.get('h1[data-test="titulo-principal"')
-      .should('exist')
-      .contains(
-        'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
-      );
+    cy.getByDataTest('titulo-principal').contains(
+      'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
+    );
   });
 });
--- cypress/bytebank/front-end/cypress/support/commands.js
+++ cypress/bytebank/front-end/cypress/support/commands.js
@@ -1,25 +1,6 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// Este arquivo é usado para criar comandos personalizados com o cypress.
+// Sintaxe: Cypress.Commands.add("nome do comando", callback).
+
+Cypress.Commands.add('getByDataTest', (selector) => {
+  return cy.get(`[data-test="${selector}"]`);
+});
