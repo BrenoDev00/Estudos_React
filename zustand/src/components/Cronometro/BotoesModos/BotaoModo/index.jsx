87605a48aeb791fe25f4d3e8e27d8421e5f8b8c1
Breno Silva Gonçalves <brenosilvagoncalves12@gmail.com>
2026-02-03 14:20:48 -0300

    adiciona reatividade ao botão modo clicado

--- zustand/src/components/Cronometro/BotoesModos/BotaoModo/index.jsx
+++ zustand/src/components/Cronometro/BotoesModos/BotaoModo/index.jsx
@@ -1,10 +1,20 @@
 import styles from "./styles.module.css";
+import { useCronometroStore } from "../../../../stores/use-cronometro-store";
+
 export default function BotaoModo({ children, modoBotao }) {
+  const modoCronometro = useCronometroStore((estado) => estado.modoCronometro);
+  const setModoCronometro = useCronometroStore(
+    (estado) => estado.setModoCronometro,
+  );
+
+  const ativo = modoBotao.id === modoCronometro.id;
+
   return (
     <button
+      onClick={() => setModoCronometro(modoBotao)}
       className={`
         ${styles["cronometer-modes__button"]}
-        ${styles["cronometer-modes__button--active"]}
+        ${ativo && styles["cronometer-modes__button--active"]}
       `}
     >
       {children}
